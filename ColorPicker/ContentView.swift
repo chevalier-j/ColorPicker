f865a268a7ac5f4f442fb2ab7bf46941e3ed63e8
username <adelgibert@gmail.com>
2022-07-20 17:22:23 +1200

    Added slider, image, red/green/blue vars

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State var color: Color
+//    @Binding var re
+    @State var red = 1.0
+    var green = 0.0
+    var blue = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Colour Slider v0.1")
+                .font(.largeTitle)
+                .padding()
+            Image(systemName: "slider.horizontal.3")
+                .foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0, opacity: 1.0))
+                .font(.largeTitle)
+                .padding()
+            Slider(value: $red, in: 0...1, step: 0.01)
+                .padding()
+        }
     }
 }
 
